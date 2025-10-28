32c964d71d29b39d2baf451934824fd0e0da6ff1
MF-Gua <tobiasagerbek6@gmail.com>
2025-10-28 15:36:36 +0100

    Sprint 1 (For real this time)

---
+++ src/main/java/org/example/madbroen/MadbroenApplication.java
@@ -0,0 +1,13 @@
+package org.example.madbroen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadbroenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MadbroenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/madbroen/MadbroenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.madbroen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadbroenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
