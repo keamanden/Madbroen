f73831fedae8bfaa9ce6cc391a06b1f6b08f8f9d
MF-Gua <tobiasagerbek6@gmail.com>
2025-10-30 13:41:18 +0100

    Lavet en header og controller

--- src/main/java/org/example/madbroen/MadbroenApplication.java
+++ src/main/java/org/example/madbroen/MadbroenApplication.java
@@ -2,8 +2,12 @@ package org.example.madbroen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+
 public class MadbroenApplication {
 
     public static void main(String[] args) {
@@ -11,3 +15,5 @@ public class MadbroenApplication {
     }
 
 }
+
+
---
+++ src/main/java/org/example/madbroen/WebController.java
@@ -0,0 +1,16 @@
+package org.example.madbroen.WebController;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+    @Controller
+    public class WebController {
+
+        @GetMapping("/index")
+        public String index() {
+            return "index";
+        }
+    }
