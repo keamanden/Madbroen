42af625d44a2afe49e089ddc67829ad56e13420c
Keamanden <than0002@stud.kea.dk>
2025-10-30 12:45:18 +0100

    tilføjet test objects og testet ret mine oplysninger

--- src/main/java/org/example/madbroen/WebController/WebController.java
+++ src/main/java/org/example/madbroen/WebController/WebController.java
@@ -1,7 +1,11 @@
 package org.example.madbroen.WebController;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.example.madbroen.TestUser;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class WebController {
@@ -12,6 +16,19 @@ public class WebController {
     @GetMapping("/dashboard")
     public String index() {
         return "pages/dashboard";
+    }
+
+    @GetMapping("/test")
+    public String showTestPage(Model model) {
+        model.addAttribute("user", new TestUser("Acme A/S", "Main Street 1", "info@acme.dk",
+                12345678, 87654321, "John Doe",
+                "donorCompany", 42, true, "Some input"));
+        model.addAttribute("userType", "donorCompany");
+        return "pages/dashboard";
+    }
+    @GetMapping("/hello")
+    public String hello() {
+        return "pages/test";
     }
 
 
