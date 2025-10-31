165d73fd53996956e838d8f42746fceb7869bea6
Keamanden <than0002@stud.kea.dk>
2025-10-31 10:07:56 +0100

    testet at listerne virker når det er distributionCompany

--- src/main/java/org/example/madbroen/WebController/WebController.java
+++ src/main/java/org/example/madbroen/WebController/WebController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class WebController {
 
@@ -26,8 +29,97 @@ public class WebController {
     public String showTestPage(Model model) {
         model.addAttribute("user", new TestUser("Acme A/S", "Main Street 1", "info@acme.dk",
                 12345678, 87654321, "John Doe",
-                "soupKitchen", 42, true, "Some input"));
+                "distributionCompany", 42, true, "Some input"));
 
+        List<TestUser> donorCompanys = new ArrayList<TestUser>();
+        List<TestUser> soupKitchens = new ArrayList<TestUser>();
+       donorCompanys.add(new TestUser(
+                "Acme A/S",
+                "Side Street 1",
+                "info@acme.dk",
+                12345678,
+                87654321,
+                "John Doe",
+                "donorCompany",
+                1,          // palletCount
+                true,
+                "First test input"
+        ));
+
+        donorCompanys.add(new TestUser(
+                "Cargo ApS",
+                "Warehouse Lane 7",
+                "support@cargo.dk",
+                99887766,
+                55667788,
+                "Peter Jensen",
+                "donorCompany",
+                5,          // palletCount
+                true,
+                "Third test input"
+        ));
+
+
+        donorCompanys.add(new TestUser(
+                "Cargo ApS",
+                "Warehouse Lane 7",
+                "support@cargo.dk",
+                99887766,
+                55667788,
+                "Peter Jensen",
+                "donorCompany",
+                5,          // palletCount
+                true,
+                "Third test input"
+        ));
+
+
+        soupKitchens.add(new TestUser(
+                "Acme A/S",
+                "High Street 1",
+                "info@acme.dk",
+                12345678,
+                87654321,
+                "John Doe",
+                "soupKitchen",
+                1,          // palletCount
+                true,
+                "First test input"
+        ));
+
+        soupKitchens.add(new TestUser(
+                "Cargo ApS",
+                "Long Lane 7",
+                "support@cargo.dk",
+                99887766,
+                55667788,
+                "Peter Jensen",
+                "soupKitchen",
+                5,          // palletCount
+                true,
+                "Third test input"
+        ));
+
+
+        soupKitchens.add(new TestUser(
+                "Cargo ApS",
+                "Short Lane 7",
+                "support@cargo.dk",
+                99887766,
+                55667788,
+                "Peter Jensen",
+                "soupKitchen",
+                5,          // palletCount
+                true,
+                "Third test input"
+        ));
+
+
+
+
+        model.addAttribute("soupKitchens", soupKitchens);
+        model.addAttribute("donorCompanys", donorCompanys);
+        model.addAttribute("users", new TestUser());
         return "pages/dashboard";
     }
 
